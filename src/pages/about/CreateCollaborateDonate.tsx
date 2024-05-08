efdaa70c7c7bfe92b0c9bddeebc012c2af32ecf9
fachri achmad maulana <fachri57@yahoo.com>
2024-05-08 17:50:18 +0700

    modified about page

--- src/pages/about/CreateCollaborateDonate.tsx
+++ src/pages/about/CreateCollaborateDonate.tsx
@@ -6,8 +6,8 @@ const CreateCollaborateDonate = () => {
       <div className="section-content">
         <div className="texts font-instrument flex justify-center gap-3 text-3xl md:text-6xl lg:text-8xl">
           <h1>Create,</h1>
-          <h1>Collaborate,</h1>
-          <h1>Donate</h1>
+          <h1>Donate,</h1>
+          <h1>Collaborate</h1>
         </div>
       </div>
     </PageSection>

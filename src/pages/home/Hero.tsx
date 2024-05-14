26703624762f39bf4b4c414448b8fe17b9ade88c
fachri achmad maulana <fachri57@yahoo.com>
2024-05-14 16:19:30 +0700

    minor hero animation changes

--- src/pages/home/Hero.tsx
+++ src/pages/home/Hero.tsx
@@ -8,14 +8,14 @@ const Hero = () => {
     setHover(prevHover => !prevHover);
   }
 
-  console.log(hover)
-
   useLayoutEffect(() => {
     const context = gsap.context(() => {
-      gsap.fromTo('.hero-texts', {
-        x: -1000,     
+      gsap.fromTo('.hero-text', {
+        y: 200,
+        opacity: 0    
       }, {
-        x: 0,
+        y: 0,
+        opacity: 1,
         duration: 1
       });
 
@@ -37,13 +37,14 @@ const Hero = () => {
       duration: 0.5
     })
   }, [hover])
+  
   return ( 
-    <section className="hero-section relative px-[5%] lg:px-[2%] py-48 bg-slate-200">
+    <section className="hero-section relative px-[5%] lg:px-[2%] py-36 bg-slate-200">
       <div className="section-content relative flex">
         <div className="hero-texts flex flex-col gap-6 w-full overflow-hidden">
-          <h1 className="text-5xl sm:text-6xl lg:text-8xl font-redhatdisplay ">Share <span onMouseEnter={hoverHandler} onMouseLeave={hoverHandler} className="small-text text-3xl">with people</span>,</h1>
-          <h1 className="text-5xl sm:text-6xl lg:text-8xl font-redhatdisplay ms-24">Care <span onMouseEnter={hoverHandler} onMouseLeave={hoverHandler} className="small-text text-3xl">for people</span>,</h1>
-          <h1 className="text-5xl sm:text-6xl lg:text-8xl font-redhatdisplay ms-40">For The Better <span onMouseEnter={hoverHandler} onMouseLeave={hoverHandler} className="small-text text-3xl">of people</span>.</h1>
+          <h1 className="hero-text text-5xl sm:text-6xl lg:text-8xl font-redhatdisplay ">Share <span onMouseEnter={hoverHandler} onMouseLeave={hoverHandler} className="small-text text-3xl">with people</span>,</h1>
+          <h1 className="hero-text text-5xl sm:text-6xl lg:text-8xl font-redhatdisplay ms-24">Care <span onMouseEnter={hoverHandler} onMouseLeave={hoverHandler} className="small-text text-3xl">for people</span>,</h1>
+          <h1 className="hero-text text-5xl sm:text-6xl lg:text-8xl font-redhatdisplay ms-40">For The Better <span onMouseEnter={hoverHandler} onMouseLeave={hoverHandler} className="small-text text-3xl">of people</span>.</h1>
         </div>
       </div>
     </section>

2cd71fd8bb5a3913134425b7a7c03a4e9a94bdfa
fachri achmad maulana <fachri57@yahoo.com>
2024-05-28 15:27:29 +0700

    fixed lazy load imports

--- src/pages/home/Index.tsx
+++ src/pages/home/Index.tsx
@@ -1,15 +1,13 @@
 // import Charities from './Charities';
 // import StartCharity from './StartCharity';
 import Hero from './Hero';
-// import AboutCharityhub from './AboutCharityhub';
-// import CreateDonateCollaborate from './CreateDonateCollaborate';
+import AboutCharityhub from './AboutCharityhub';
+import CreateDonateCollaborate from './CreateDonateCollaborate';
 import PreLoadingOverlay from '../../components/PreLoadingOverlay';
 import { Suspense, lazy } from 'react';
 
-const AboutCharityhub = lazy(() => import('./AboutCharityhub'));
-const CreateDonateCollaborate = lazy(() => import('./CreateDonateCollaborate'));
- const Charities = lazy(() => import('./Charities'));
- const StartCharity = lazy(() => import('./StartCharity'));
+const Charities = lazy(() => import('./Charities'));
+const StartCharity = lazy(() => import('./StartCharity'));
 
 
 const Home = () => {
@@ -17,12 +15,8 @@ const Home = () => {
     <>
       <PreLoadingOverlay overlayText="charityhub."/>
       <Hero />
-      <Suspense fallback={<div>Loading...</div>}>
-        <AboutCharityhub />
-      </Suspense>
-      <Suspense fallback={<div>Loading...</div>}>
-        <CreateDonateCollaborate />
-      </Suspense>
+      <AboutCharityhub />
+      <CreateDonateCollaborate />
       <Suspense fallback={<div>Loading...</div>}>
         <Charities />
       </Suspense>
--- src/routes/RouteHandler.tsx
+++ src/routes/RouteHandler.tsx
@@ -2,37 +2,51 @@ import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } 
 import RootLayout from './RootLayout';
 import { lazy, Suspense } from 'react';
 
-// const ContactUs = lazy(() => import('../pages/contactus/Index'));
-// const Charities = lazy(() => import('../pages/charities/Index'));
-const CharityDetail = lazy(() => import('../pages/CharityDetail/Index'));
-
-import NotAuthorized from '../pages/notAuthorized/Index';
 import Home from '../pages/home/Index';
-import Charities from '../pages/charities/Index';
-import ContactUs from '../pages/contactus/Index';
-import Login from '../pages/login/Index';
-import Register from '../pages/register/Index';
-import Dashboard from '../pages/dashboard/Index';
-import CreateCharity from '../pages/createCharity/Index';
-import EditCharity from '../pages/editCharity/Index';
-import Donate from '../pages/donate/Index';
+
+const NotAuthorized = lazy(() => import('../pages/notAuthorized/Index'));
+const ContactUs = lazy(() => import('../pages/contactus/Index'));
+const Charities = lazy(() => import('../pages/charities/Index'));
+const CharityDetail = lazy(() => import('../pages/CharityDetail/Index'));
+const Login = lazy(() => import('../pages/login/Index'));
+const Register = lazy(() => import('../pages/register/Index'));
+const Dashboard = lazy(() => import('../pages/dashboard/Index'));
+const CreateCharity = lazy(() => import('../pages/createCharity/Index'));
+const EditCharity = lazy(() => import('../pages/editCharity/Index'));
+const Donate = lazy(() => import('../pages/donate/Index'));
 
 const RouteHandler = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<RootLayout />}>
-        <Route path="not-authorized" element={<NotAuthorized />}/>
+        <Route path="not-authorized" element={
+          <Suspense fallback={<div className="bg-slate-200"></div>}>
+            <NotAuthorized />
+          </Suspense>
+        }/>
         <Route index element={
           <Home />
         } />
         <Route path="charities" element={
-          <Charities />
+          <Suspense fallback={<div className="bg-slate-200"></div>}>
+            <Charities />
+          </Suspense>
         } />
         <Route path="contact-us" element={
-          <ContactUs />
+          <Suspense fallback={<div className="bg-slate-200"></div>}>
+            <ContactUs />
+          </Suspense>
         }/>
-        <Route path="register" element={<Register />}/>
-        <Route path="login" element={<Login />}/>
+        <Route path="register" element={
+          <Suspense fallback={<div className="bg-slate-200"></div>}>
+            <Register />
+          </Suspense>
+        }/>
+        <Route path="login" element={
+          <Suspense fallback={<div className="bg-slate-200"></div>}>
+            <Login />
+          </Suspense>
+        }/>
 
         <Route path="charity/">
           <Route path=":charityId" element={
@@ -51,9 +65,21 @@ const RouteHandler = () => {
         </Route>
 
         <Route path=":email/">
-          <Route path="dashboard" element={<Dashboard />}/>
-          <Route path="create-charity" element={<CreateCharity />}/>
-          <Route path=":charityId/edit-charity" element={<EditCharity />}/>
+          <Route path="dashboard" element={
+            <Suspense fallback={<div>Loading</div>}>
+              <Dashboard />
+            </Suspense>
+          }/>
+          <Route path="create-charity" element={
+            <Suspense fallback={<div>Loading</div>}>
+              <CreateCharity />
+            </Suspense>
+          }/>
+          <Route path=":charityId/edit-charity" element={
+            <Suspense fallback={<div>Loading</div>}>
+              <EditCharity />
+            </Suspense>
+          }/>
         </Route>
       </Route>
     )
